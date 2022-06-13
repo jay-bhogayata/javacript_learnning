3994df58b276efbc8c96337d7b5e852308ddc193
JayBhogayata <jaybhogayata53@gmail.com>
2022-06-13 11:32:05 +0530

    simple counter using settimeout and setintervel

---
+++ 04 DOM/proj1/script.js
@@ -0,0 +1,16 @@
+var counter = document.querySelector(".counter");
+let followers = document.querySelector(".followers");
+
+
+var count = 1;
+
+setInterval(()=>{
+    if(count < 1000){
+        count++;
+        counter.innerText = count;
+    }
+},1)
+
+setTimeout(()=>{
+    followers.innerHTML = "followers on instagram";
+},5000)
